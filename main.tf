d5509b5e41d57fcc5a3ceca85b04f86e6df3d171
Michael Moyle <michaeljmoyle@gmail.com>
2023-11-16 10:35:34 +0100

    add files

---
+++ main.tf
@@ -0,0 +1,67 @@
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags =  {
+    Name = "VPC-01"
+  }
+}
+
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags =  {
+    Name = "public-${data.aws_availability_zones.available.names[count.index]}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.${count.index + 2}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags =  {
+    Name = "private-${data.aws_availability_zones.available.names[count.index]}"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
